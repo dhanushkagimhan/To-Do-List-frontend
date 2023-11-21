437f1ba8f5da6ff4e682bd53813dcbecd43b51cb
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-11-21 22:22:10 +0530

    start frontend development

---
+++ src/pages/CreateTask.tsx
@@ -0,0 +1,7 @@
+export default function CreateTask() {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <div className="mt-20 ">
+                <h1>To Do Task List</h1>
+                <div className=" text-red-600"></div>
+            </div>
+        </div>
+    );
+}
